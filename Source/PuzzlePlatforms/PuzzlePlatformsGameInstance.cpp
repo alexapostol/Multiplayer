9efd13ece8826ab111745a9e74f319d9400cfa2c
geostik <cgiteach@gmail.com>
2017-11-23 18:33:15 +0200

    Hosting with ServerTravel, Joining with ClientTravel
    Add Lobby, call them
    
    Only -game is ok now
    
    PS C:\Users\Alex> & "C:\Program Files\Epic
    Games\UE_4.18\Engine\Binaries\Win64\UE4Editor.exe"
    "C:\Users\Alex\Desktop\Mul
    
    tiplayer\Multiplayer\PuzzlePlatforms.uproject"/game/Maps/ThirdPersonExampleMap
    -server -log
    PS C:\Users\Alex> & "C:\Program Files\Epic
    Games\UE_4.18\Engine\Binaries\Win64\UE4Editor.exe"
    "C:\Users\Alex\Desktop\Mul
    tiplayer\Multiplayer\PuzzlePlatforms.uproject" Ip -game -log

--- Source/PuzzlePlatforms/PuzzlePlatformsGameInstance.cpp
+++ Source/PuzzlePlatforms/PuzzlePlatformsGameInstance.cpp
@@ -20,6 +20,11 @@ void UPuzzlePlatformsGameInstance::Host()
 	if (!ensure(Engine != nullptr)) return;
 
 	Engine->AddOnScreenDebugMessage(0, 2, FColor::Green, TEXT("Hosting")); // arg0  0 override message
+
+	UWorld* World = GetWorld();
+	if (!ensure(World!=nullptr)) return;
+
+	World->ServerTravel("/Game/Maps/ThirdPersonExampleMap?listen"); // default listen -server
 }
 
 void UPuzzlePlatformsGameInstance::Join(const FString& Address)
@@ -27,5 +32,10 @@ void UPuzzlePlatformsGameInstance::Join(const FString& Address)
 	UEngine* Engine = GetEngine();
 	if (!ensure(Engine != nullptr)) return;
 
-	Engine->AddOnScreenDebugMessage(0, 2, FColor::Green, FString::Printf(TEXT("Joining %s",*Address))); // arg0  -1 don't override message
+	Engine->AddOnScreenDebugMessage(0, 2, FColor::Green, FString::Printf(TEXT("Joining %s"),*Address)); // arg0  -1 don't override message
+
+	APlayerController* PlayerControllor = GetFirstLocalPlayerController();
+	if (!ensure(PlayerControllor != nullptr)) return;
+	PlayerControllor->ClientTravel(Address,ETravelType::TRAVEL_Absolute);
+
 }
