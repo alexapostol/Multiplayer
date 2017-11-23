74a87f0cf9f1db871124020d3c27c613ad50eed7
geostik <cgiteach@gmail.com>
2017-11-23 16:53:55 +0200

    Activating platforms from tiggers
    Add ActivePlatforms counter in MovingPlatform
    Add array in PlatformTriggrt

--- Source/PuzzlePlatforms/PlatformTrigger.h
+++ Source/PuzzlePlatforms/PlatformTrigger.h
@@ -4,7 +4,6 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
-#include "Components/BoxComponent.h"
 #include "PlatformTrigger.generated.h"
 
 
@@ -29,8 +28,11 @@ private:
 	UPROPERTY(VisibleAnywhere)
 		class UBoxComponent* TriggerVolume;
 
+	UPROPERTY(EditAnywhere)
+		TArray<class AMovingPlatform*> PlatformsToTrigger;
+
 	UFUNCTION()
 		void OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
 	UFUNCTION()
-	void OnOverlapEnd(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+		void OnOverlapEnd(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
 };
