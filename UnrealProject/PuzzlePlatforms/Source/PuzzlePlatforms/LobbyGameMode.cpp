d2a040bc8eafe1ef4e168744082e60ca983f4ef3
geostik <cgiteach@gmail.com>
2017-11-29 13:57:07 +0200

    Enabling Seamless Travel

--- UnrealProject/PuzzlePlatforms/Source/PuzzlePlatforms/LobbyGameMode.cpp
+++ UnrealProject/PuzzlePlatforms/Source/PuzzlePlatforms/LobbyGameMode.cpp
@@ -5,14 +5,19 @@
 
 void ALobbyGameMode::PostLogin(APlayerController* NewPlayer)
 {
+	Super::PostLogin(NewPlayer); //call class
 	++NumberOfPlayers;
 	if(NumberOfPlayers>=3)
 	{
-		UE_LOG(LogTemp, Warning, TEXT("Reached 3 players!"));
+		UWorld* World = GetWorld();
+		if (!ensure(World != nullptr)) return;
+		bUseSeamlessTravel = true;
+		World->ServerTravel("/Game/PuzzlePlatforms/Maps/Game?listen");
 	}
 }
 
 void ALobbyGameMode::Logout(AController* Exiting)
 {
+	Super::Logout(Exiting);
 	--NumberOfPlayers;
 }
